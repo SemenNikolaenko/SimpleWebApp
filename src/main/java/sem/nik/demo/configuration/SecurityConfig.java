b3461476ff15d862311f6245e5132e084cf0cb78
Семен Николаенко <nikolaenko.semen12@gmail.com>
2021-01-08 10:24:59 +0300

    1

--- src/main/java/sem/nik/demo/configuration/SecurityConfig.java
+++ src/main/java/sem/nik/demo/configuration/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/products/show/**").hasAnyRole("ADMIN")
+                .antMatchers("/products/edit/**").hasAnyRole("ADMIN")
                 .anyRequest().permitAll()
                 .and().formLogin()
                 .permitAll();
